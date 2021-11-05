225c6ffd2548372ffa98d974333caf28a91d2a5d
Abdullah Habes <abdulla096@hotmail.com>
2021-11-05 06:00:33 +0000

    refactor breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -4,7 +4,10 @@ const request = require('request');
 const fetchBreedDescription = (url, callback) => {
   const uri = 'https://api.thecatapi.com/v1/breeds/search?q=' + url;
   request(uri, (error, response, body) => {
-    callback(error, JSON.parse(body)[0].description);
+    if (error) callback(error, null);
+    if (response.statusCode !== 200)
+      callback(Error(`The request was not successful, code ${response.statusCode}`), null);
+    callback(null, JSON.parse(body)[0].description);
   });
 
 };
